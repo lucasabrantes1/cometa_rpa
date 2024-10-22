0b4d8bfce06c1b361227e00585010aa8eba69fc0
Abrantes <lucas.abrantes@kuehne-nagel.com>
2024-10-22 08:37:25 -0300

    Remove webdrive wait until

--- cometa_rpa.py
+++ cometa_rpa.py
@@ -1,8 +1,6 @@
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 import time
 
@@ -22,35 +20,40 @@ wd_chrome = webdriver.Chrome(options=chrome_option)
 url = 'https://www.viacaocometa.com.br/'
 wd_chrome.get(url)
 wd_chrome.set_window_size(1392, 1104)
-departure_input = WebDriverWait(wd_chrome, 10).until(
-    EC.element_to_be_clickable((By.ID, "input-departure"))
-)
+
+# Interações com time.sleep
+time.sleep(4)
+departure_input = wd_chrome.find_element(By.ID, "input-departure")
 departure_input.click()
-time.sleep(1)
+time.sleep(4)
 departure_input.send_keys("São Paulo (Rod. Tietê) (SP)")
-time.sleep(2)
-dropdown_option = WebDriverWait(wd_chrome, 10).until(
-    EC.element_to_be_clickable((By.XPATH, "//li[contains(., 'São Paulo (Rod. Tietê) (SP)')]"))
-)
+time.sleep(5)
+
+dropdown_option = wd_chrome.find_element(By.XPATH, "//li[contains(., 'São Paulo (Rod. Tietê) (SP)')]")
 dropdown_option.click()
-destination_input = WebDriverWait(wd_chrome, 10).until(
-    EC.element_to_be_clickable((By.ID, "input-destination"))
-)
+time.sleep(4)
+
+destination_input = wd_chrome.find_element(By.ID, "input-destination")
 destination_input.click()
-time.sleep(1)
+time.sleep(4)
 destination_input.send_keys("Belo Horizonte (MG)")
-time.sleep(2)
-dropdown_option_dest = WebDriverWait(wd_chrome, 10).until(
-    EC.element_to_be_clickable((By.XPATH, "//li[contains(., 'Belo Horizonte (MG)')]"))
-)
+time.sleep(4)
+
+dropdown_option_dest = wd_chrome.find_element(By.XPATH, "//li[contains(., 'Belo Horizonte (MG)')]")
 dropdown_option_dest.click()
+time.sleep(4)
+
 wd_chrome.find_element(By.ID, "input-date").click()
 time.sleep(1)
 wd_chrome.find_element(By.ID, "input-date").send_keys("21/10/2024")
 wd_chrome.find_element(By.ID, "input-date").send_keys(Keys.ENTER)
+time.sleep(2)
+
 wd_chrome.find_element(By.ID, "input-date-return").click()
 time.sleep(1)
 wd_chrome.find_element(By.ID, "input-date-return").send_keys("28/10/2024")
 wd_chrome.find_element(By.ID, "input-date-return").send_keys(Keys.ENTER)
+time.sleep(2)
+
 wd_chrome.find_element(By.ID, "search-button").click()
 time.sleep(30)
